1c2b1c1b2b93866744f946184e9ea54ab5cdca40
javlonbeksaidov-developer <javlonbeksaidov09@gmail.com>
2026-06-21 19:54:26 +0500

    create tasks{06..09}.py

---
+++ tasks08.py
@@ -0,0 +1,25 @@
+
+'''
+Meni zanjirband qil
+
+Umumiy funksiya zanjirini yozing
+Boshlang'ich qiymatni qabul qiladigan umumiy funksiya zanjirini va unda bajariladigan funksiyalar massivini (Ruby uchun belgilar massivi) yozing.
+
+Har bir funksiya uchun kirish ma'lumotlari oldingi funksiyaning chiqishidir (boshlang'ich qiymatni o'zining kirish ma'lumotlari sifatida qabul qiladigan birinchi funksiyadan tashqari). Bajarilgandan so'ng yakuniy qiymatni qaytaring.
+
+def add10(x): return x + 10
+def mul30(x): return x * 30
+
+chain(50, [add10, mul30])
+# returns 1800
+'''
+
+def chain(init_val, functions):
+    current_value = init_val
+    
+    # Ro'yxatdagi har bir funksiyani ketma-ket chaqiramiz
+    for func in functions:
+        # joriy qiymatni funksiyaga beramiz va yangi qiymatni saqlaymiz
+        current_value = func(current_value)
+        
+    return current_value
---
+++ tasks09.py
@@ -0,0 +1,33 @@
+
+'''
+Evaluate a Polynomial Efficiently
+
+
+Given an array of coefficients representing a polynomial and a value x, compute the value of the polynomial. The coefficients are ordered from the highest degree term to the constant term.
+
+For Example: [2,3,4]
+
+Represent: 2x² + 3x + 4
+
+Task:
+Implement a function that evaluates the polynomial at the given value of x.
+
+Examples:
+evaluate([2, 3, 4], 2) == 18 because 2·2² + 3·2 + 4 = 8 + 6 + 4 = 18.
+
+Notes:
+The polynomial may contain a large number of coefficients.
+Coefficients may be positive, negative, or zero.
+x may be positive, negative, or zero.
+Your solution should be efficient enough to handle large inputs.
+'''
+
+def evaluate_polynomial(coefficients: list[int], x: int) -> int:
+    total = []
+    uzunlik = len(coefficients)
+    for i in coefficients:
+        ifoda = i * pow(x, uzunlik - 1)
+        total.append(ifoda)
+        uzunlik -= 1
+    
+    return sum(total)
